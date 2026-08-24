3cf1836802c9ce6d2008cd2f254e917f8b963b30
yanlucas <yanlucas0702@gmail.com>
2026-08-24 16:43:34 -0300

    Refactor Guerreiro attack logic for clarity
    Updated the attack logic to clarify shield usage in damage calculation.

--- Guerreiro.java
+++ Guerreiro.java
@@ -20,13 +20,13 @@ public class Guerreiro extends Personagem {
     public void executarAtaqueEspecial(Personagem alvo) {
         int danoBase = 10;
         
-        // Exemplo de lógica: o Guerreiro usa o escudo para aumentar o dano do ataque
+        // o escudo sera usando pra reduzi o dano do guerreiro
         int danoFinal = danoBase + (this.escudo / 5);
 
         // Aplica o dano ao alvo
         alvo.setHpAtual(alvo.getHpAtual() - danoFinal);
 
-        // Exibe a mensagem do ataque
+        // Exibe a mensagem do ataque com o dano ja reduzido
         System.out.println(getNome() + " atacou " + alvo.getNome() + " causando " + danoFinal + " de dano (bônus de escudo aplicado)!");
     }
 }
